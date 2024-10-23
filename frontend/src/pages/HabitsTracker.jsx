14bd611def572b41b41aae160904d7467fed060c
leo leroy <leo.langlois123@gmail.com>
2024-10-23 16:22:31 +0200

    icone

--- frontend/src/pages/HabitsTracker.jsx
+++ frontend/src/pages/HabitsTracker.jsx
@@ -8,15 +8,15 @@ function HabitsTacker() {
         <h2 className="titreBloc">Aujourd'hui</h2>
 
         <div className="barreTracker">
-          <img className="iconeHabits" src="public/glass-water-droplet-solid.svg" alt="glass of water icon" />
+          <img className="iconeHabits" src="glass-water-droplet-solid.svg" alt="glass of water icon" />
           <p className="pHabits">Eau</p>
-          <button className="boutonHabits"><img className="iconeHabits" src="public/plus-solid.svg" alt="plus icon"/></button>
+          <button className="boutonHabits"><img className="iconeHabits" src="plus-solid.svg" alt="plus icon"/></button>
         </div>
 
         <div className="barreTracker">
-          <img className="iconeHabits" src="public/dumbbell-solid.svg" alt="dumbbell icon" />
+          <img className="iconeHabits" src="dumbbell-solid.svg" alt="dumbbell icon" />
           <p className="pHabits">Sport</p>
-          <button className="boutonHabits"><img className="iconeHabits" src="public/plus-solid.svg" alt="plus icon"/></button>
+          <button className="boutonHabits"><img className="iconeHabits" src="plus-solid.svg" alt="plus icon"/></button>
         </div>
 
       </article>
@@ -25,15 +25,15 @@ function HabitsTacker() {
       <h2 className="titreBloc">Cette semaine</h2>
 
       <div className="barreTracker">
-          <img className="iconeHabits" src="public/shoe-prints-solid.svg" alt="glass of water icon" />
+          <img className="iconeHabits" src="shoe-prints-solid.svg" alt="glass of water icon" />
           <p className="pHabits">Pas</p>
-          <button className="boutonHabits"><img className="iconeHabits" src="public/plus-solid.svg" alt="plus icon"/></button>
+          <button className="boutonHabits"><img className="iconeHabits" src="plus-solid.svg" alt="plus icon"/></button>
         </div>
 
         <div className="barreTracker">
-          <img className="iconeHabits" src="public/moon-solid.svg" alt="glass of water icon" />
+          <img className="iconeHabits" src="moon-solid.svg" alt="glass of water icon" />
           <p className="pHabits">Sommeil</p>
-          <button className="boutonHabits"><img className="iconeHabits" src="public/plus-solid.svg" alt="plus icon"/></button>
+          <button className="boutonHabits"><img className="iconeHabits" src="plus-solid.svg" alt="plus icon"/></button>
         </div>
         
       </article>
