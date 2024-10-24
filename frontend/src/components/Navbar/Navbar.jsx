295290111478045a6b11d3e0fea927d9e7776a55
leo leroy <leo.langlois123@gmail.com>
2024-10-24 16:30:18 +0200

    navbar

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -11,7 +11,7 @@ function Navbar() {
         <li className="navbarLink"><a href="#"><i class="bi bi-calendar4"></i></a></li>
         <li className="navbarLink"><a href="detailprojet"><i class="bi bi-plus-circle"></i></a></li>
         <li className="navbarLink"><a href="habitstracker"><i class="bi bi-star-fill"></i></a></li>
-        <li className="navbarLink"><a href="#"><i class="bi bi-person"></i></a></li>
+        <li className="navbarLink"><a href="login"><i class="bi bi-person"></i></a></li>
       </ul>
     </nav>
     </>
