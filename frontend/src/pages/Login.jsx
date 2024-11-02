07880c92b5890cc1f73e2d6ffb7f195c5bab1542
elbitoku <leo.langlois123@gmail.com>
2024-11-02 18:26:00 +0100

    redir

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -40,7 +40,7 @@ function Login() {
       await login({ email, password });
       
       // Si la connexion réussit, redirection vers la page d'accueil
-      navigate('/');
+      navigate('/profile');
     } catch (err) {
       // En cas d'erreur, affichage dans la console
       console.error(err);
