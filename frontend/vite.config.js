638787203fd02308d993e181387f024005d2eadf
elbitoku <leo.langlois123@gmail.com>
2024-11-03 21:47:20 +0100

    manifest

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,21 +5,20 @@ import { VitePWA } from 'vite-plugin-pwa'
 export default defineConfig({
   plugins: [
     react(),
-    
-    // Rendre l'application PWA (Progressive Web App),
-    // celle-ci pourra être installé en tant qu'application sur un mobile
     VitePWA({
       registerType: 'autoUpdate',
       includeAssets: [
         'favicon.png',
         'robots.txt',
         'apple-touch-icon.png',
+        'logo.png',
       ],
       manifest: {
         name: 'ToDo List',
         short_name: 'App',
         description: 'Projet ToDo List en groupe',
-        theme_color: '#ffffff',
+        theme_color: '#2eaebf',
+        background_color: '#2eaebf', 
         icons: [
           {
             src: 'favicon.png',
@@ -30,8 +29,15 @@ export default defineConfig({
             src: 'favicon.png',
             sizes: '512x512',
             type: 'image/png',
+          },
+          {
+            src: 'logo.png',
+            sizes: '1024x1024', 
+            type: 'image/png',
+            purpose: 'any maskable' 
           }
-        ]
+        ],
+        splash_pages: null
       }
     })
   ],
